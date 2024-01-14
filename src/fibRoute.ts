22de3369e3745184f7e4f0650af8499dfd5572da
John Boyd <46943648+johnb254@users.noreply.github.com>
2024-01-14 13:52:05 -0500

    Update fibRoute.ts
    File now imports types from express package

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,12 +1,13 @@
 // Endpoint for querying the fibonacci numbers
 
 import fibonacci from "./fib";
+import { Request, Response } from "express";
 
-export default (req: { params: { num: any; }; }, res: { send: (arg0: string) => void; }): void => {
+export default (req: Request, res: Response) => {
   const { num } = req.params;
 
   const fibN = fibonacci(parseInt(num));
-  let result = `fibonacci(${num}) is ${fibN}`;
+  let result: string = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
